1d887a0eed55c7d432a377bca3c9e57973d9d2e1
TianyuAaronHan <hyyhtyhty@gmail.com>
2025-10-20 14:55:13 +1100

    Add Program.cs – first commit from VS Code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-using System;
-
 namespace ProGitForProgrammers
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from ProGitForProgrammers!");
+            Console.WriteLine("Hello World!");
+           Console.WriteLine("I just added this in Visual Studio");
         }
     }
 }
